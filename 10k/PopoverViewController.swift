1d5b1c611104cfb6b6186658b8cd7df8b50bfd13
Umut Bozkurt <umutbozkurt@gmail.com>
2015-09-06 01:43:33 +0300

    change target vc with views

--- 10k/PopoverViewController.swift
+++ 10k/PopoverViewController.swift
@@ -29,28 +29,57 @@ class PopoverViewController: NSViewController
 
     @IBAction func addTarget(sender: AnyObject)
     {
-        let targetViewController = TargetViewController(nibName: "TargetViewController", bundle: NSBundle.mainBundle())
-        self.view.addSubview(targetViewController!.view)
-
-        targetViewController!.view.snp_makeConstraints { (make) -> Void in
-            let upperView: NSView!
-            
-            if self.targetViews!.count > 0
+        var loadedViews: NSArray?
+        NSBundle.mainBundle().loadNibNamed("TargetView", owner: nil, topLevelObjects: &loadedViews)
+        // our view may not be firstItem of loadedViews
+        
+        var targetView: TargetView!
+        for obj: AnyObject in loadedViews!
+        {
+            if obj.isKindOfClass(TargetView)
             {
-                upperView = self.targetViews!.lastObject as! NSView!
+                targetView = obj as! TargetView
             }
-            else
-            {
-                upperView = self.addButton
-            }
-            
-            make.centerY.equalTo(upperView.snp_centerY).offset(40.0)
-            make.centerX.equalTo(upperView.snp_centerX)
-            make.width.equalTo(450)
-            make.height.equalTo(150)
         }
         
-        self.targetViews?.addObject(targetViewController!.view)
+        self.view.addSubview(targetView)
+        targetView.removeButton.target = self
+        targetView.removeButton.action = Selector("removeTargetView:")
+        self.targetViews?.addObject(targetView)
+        self.arrangeTargetViews()
         self.addButton.enabled = self.shouldAddTarget()
+    }
+    
+    private func arrangeTargetViews()
+    {
+        for var i = self.targetViews!.count - 1; i >= 0; i--
+        {
+            let targetView = self.targetViews!.objectAtIndex(i) as! TargetView
+            
+            targetView.snp_remakeConstraints { (make) -> Void in
+                let upperView: NSView!
+                
+                if i == 0
+                {
+                    upperView = self.addButton
+                }
+                else
+                {
+                    upperView = self.targetViews!.objectAtIndex(i - 1) as! NSView
+                }
+                
+                make.centerY.equalTo(upperView.snp_centerY).offset(40.0)
+                make.centerX.equalTo(upperView.snp_centerX)
+                make.width.equalTo(450)
+                make.height.equalTo(150)
+            }
+        }
+    }
+    
+    func removeTargetView(targetView: TargetView)
+    {
+        self.targetViews?.removeObject(targetView)
+        targetView.removeFromSuperview()
+        self.arrangeTargetViews()
     }
 }
---
+++ 10k/TargetView.swift
@@ -0,0 +1,15 @@
+//
+//  TargetView.swift
+//  10k
+//
+//  Created by Umut Bozkurt on 05/09/15.
+//  Copyright (c) 2015 Umut Bozkurt. All rights reserved.
+//
+
+import Cocoa
+
+class TargetView: NSView
+{
+    @IBOutlet var subjectTextField: NSTextField!
+    @IBOutlet var removeButton: NSButton!
+}
