16d338c4998c841e25e037a737972dd2ccc4d274
Umut Bozkurt <umutbozkurt@gmail.com>
2015-09-12 18:53:20 +0300

    selectabe table view applications

--- 10k/SubjectAppsViewController.swift
+++ 10k/SubjectAppsViewController.swift
@@ -14,21 +14,29 @@ class SubjectAppsViewController: NSViewController
     @IBOutlet var applicationsTableView: NSTableView!
     @IBOutlet var currentSubjectLabel: NSTextField!
     @IBOutlet var seperator: NSView!
+    @IBOutlet var nextButton: NSButton!
+    @IBOutlet var previousButton: NSButton!
     
     var applications: Array<String> = []
     var subjects: Array<String> = []
+    var subjectApplicationMap = Dictionary<String, Array<String>>()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         self.applications = Array(Set(self.fetchApplications())).sorted{ $0 < $1 }  // Aplhabetically order unique objects
+        
         self.applicationsTableView.intercellSpacing = CGSizeZero
         self.applicationsTableView.setDelegate(self)
         self.applicationsTableView.setDataSource(self)
-        self.currentSubjectLabel.stringValue = self.subjects.first!
 
+        self.currentSubjectLabel.stringValue = self.subjects.first!
+        
         self.seperator.wantsLayer = true
         self.seperator.layer?.backgroundColor = NSColor.labelColor().CGColor
+        
+        self.changeNextButton(0)
+        self.previousButton.enabled = self.shouldPromptPreviousSubject()
     }
     
     func fetchApplications() -> Array<String>
@@ -49,6 +57,51 @@ class SubjectAppsViewController: NSViewController
         }).map({(appName: String) -> String in
             return appName.stringByDeletingPathExtension
         })
+    }
+    
+    func shouldPromptNextSubject() -> Bool
+    {
+        return self.applicationsTableView.selectedRowIndexes.count > 0
+    }
+    
+    func shouldPromptPreviousSubject() -> Bool
+    {
+        return (self.subjects as NSArray).indexOfObject(self.currentSubjectLabel.stringValue) > 0
+    }
+    
+    @IBAction func promptNextSubject(sender: NSButton)
+    {
+        let currentIndex = (self.subjects as NSArray).indexOfObject(self.currentSubjectLabel.stringValue)
+        self.currentSubjectLabel.stringValue = self.subjects[currentIndex + 1]
+        self.changeNextButton(currentIndex + 1)
+        
+        self.applicationsTableView.deselectAll(nil)
+    }
+    
+    @IBAction func promptPreviousSubject(sender: NSButton)
+    {
+        self.nextButton.title = "Next"
+        self.nextButton.action = Selector("promptNextSubject:")
+        
+        let currentIndex = (self.subjects as NSArray).indexOfObject(self.currentSubjectLabel.stringValue)
+        self.currentSubjectLabel.stringValue = self.subjects[currentIndex - 1]
+        self.previousButton.enabled = self.shouldPromptPreviousSubject()
+    }
+    
+    func changeNextButton(forIndex: Int)
+    {
+        if forIndex == self.subjects.count - 1
+        {
+            self.nextButton.title =  "Done"
+            self.nextButton.action = Selector("submit:")
+        }
+        
+        self.nextButton.enabled = self.shouldPromptNextSubject()
+    }
+    
+    func submit(sender: NSButton)
+    {
+        println(self.subjectApplicationMap)
     }
 }
 
@@ -83,5 +136,16 @@ extension SubjectAppsViewController: NSTableViewDelegate, NSTableViewDataSource
     func tableView(tableView: NSTableView, heightOfRow row: Int) -> CGFloat
     {
         return 30
+    }
+    
+    func tableViewSelectionDidChange(notification: NSNotification)
+    {
+        var selectedApplications: Array<String> = []
+        self.applicationsTableView.selectedRowIndexes.enumerateIndexesUsingBlock({(index, stop) -> Void in
+            selectedApplications.append(self.applications[index])
+        })
+        self.subjectApplicationMap[self.currentSubjectLabel.stringValue] = selectedApplications
+        
+        self.nextButton.enabled = self.shouldPromptNextSubject()
     }
 }
